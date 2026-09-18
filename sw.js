2fdce3c5ba32e88790cce5868fa4ee3402cfcd89
eliesben <eliesbendjedou30@gmail.com>
2026-09-18 08:30:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const C='anim-compagnon-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE='anim-compagnon-v21';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
